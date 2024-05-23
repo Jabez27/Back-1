5600ba6130df810f6f69c7cecc28549bc20d0112
jabezbolt <55345235+jabezbolt@users.noreply.github.com>
2024-05-23 09:45:04 +0530

    authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,72 +1,48 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+//authMiidleware
+
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-router.post('/register', async (req, res) => {
+const authMiddleware = async (req, res, next) => {
   try {
-    const { username, email, password, classValue, section, rollNumber } = req.body;
+    // Check for token in headers
+    const token = req.header('Authorization');
 
-    // Validate request body
-    if (!username || !email || !password || !classValue || !section || !rollNumber) {
-      return res.status(400).json({ message: 'Username, email, and password are required' });
+    // Check if token is missing
+    if (!token) {
+      return res.status(401).json({ message: 'Authorization denied. No token provided' });
     }
 
-    // Check if user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+    const authToken = token.split(' ')[1];
+
+    try {
+      // Verify the token
+      const decodedToken = jwt.verify(authToken, process.env.JWT_SECRET);
+
+      // Check if the token has expired
+      if (decodedToken.exp < Date.now() / 6554) {
+        return res.status(401).json({ message: 'Authorization denied. Token has expired' });
+      }
+
+      // Fetch the user
+      const user = await User.findById(decodedToken.userId);
+      if (!user) {
+        return res.status(401).json({ message: 'User not found' });
+      }
+
+      // Set authToken and user in the request object
+      req.authToken = authToken;
+      req.user = user;
+
+      next(); // Call next middleware or route handler
+    } catch (error) {
+      console.error('Error authenticating user:', error.message);
+      return res.status(401).json({ message: 'Invalid token' });
     }
-
-    const newUser = new User({
-      username,
-      email,
-      password,
-      classValue,
-      section,
-      rollNumber,
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    console.error('Error registering user:', error.message);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error authenticating user:', error.message);
+    res.status(401).json({ message: 'Invalid token' });
   }
-});
+};
 
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // Validate request body
-    if (!email || !password) {
-      return res.status(400).json({ message: 'Email and password are required' });
-    }
-
-    // Check if user exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid Email' });
-    }
-
-    // Check password
-    // const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (user.password !== password) {
-      return res.status(401).json({ message: 'Invalid Password' });
-    }
-
-    // Generate token without expiration
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-
-    res.status(200).json({ token });
-  } catch (error) {
-    console.error('Error logging in:', error.message);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-module.exports = router;
+module.exports = authMiddleware;
